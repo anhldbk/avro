7f69b69cf95721b0613bef1552779fa3cf687344
Andy Le <anhld2@vng.com.vn>
2020-03-22 22:22:22 +0700

    Scripts: added a script to install this package locally

---
+++ scripts/local-install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+mvn \
+	-Dproject.parent.basedir=`pwd` \
+	-Dproject.parent.parent.basedir=`pwd` \
+	-Dproject.parent.parent.parent.basedir=`pwd` \
+	-DskipTests=true \
+	-Drat.skip=true \
+	-Dcheckstyle.skip \
+  source:jar \
+  install
